311c38b2c7fcb651e4287163837202633dd5f438
Namrata Kasaraneni <nammy.kasaraneni@gmail.com>
2022-02-23 13:58:00 -0800

    updates to character class including methods to change health, happiness, and relationships

--- CollegeRuled.py
+++ CollegeRuled.py
@@ -10,15 +10,52 @@ import pytz
 
 
 class Character:
-    def __init__(self, name, happiness, location, in_spacesuit):
+    def __init__(self, name, health, happiness):
         self.name = name
-        self.happiness = happiness
-        self.location = location
-        self.relationships = {}
-        self.in_spacesuit = in_spacesuit
+        self.health = health # scale of 0 to 10
+        self.happiness = happiness # scale of 0 to 10
+        self.has_job = False
+        self.exploited = False
+        self.murderer = False
+        self.stole = False
+        self.fugitive = False
+        self.relationships = {} # key: other character, val: [-10, 10]
+        self.location = None
+        # self.in_spacesuit = False
 
-    def updateRelationship(self, other_character, friendship_enemy_index):
-        self.relationships[other_character] = friendship_enemy_index
+    def updateRelationship(self, other_character, relationship_change):
+        """ 
+        change relationship between characters by decreasing
+        or increasing value
+        @relationship_change: int amount to change relationship by, positive or negative"""
+        current_relationship =  self.relationships[other_character]
+        new_relationship = current_relationship + relationship_change
+        if abs(new_relationship) > 10:
+            new_relationship = 10 * new_relationship/abs(new_relationship)
+
+        self.relationships[other_character] = int(new_relationship)
+    
+    def updateHealth(self, health_change):
+        """ 
+        change health of character
+        @health_change: int amount to change health by, positive or negative"""
+        new_health = self.health + health_change
+        if new_health > 10:
+            new_health = 10
+        elif new_health < 0:
+            new_health = 0
+        self.health = new_health
+    
+    def updateHappiness(self, happiness_change):
+        """ 
+        change happiness of character
+        @happiness_change: int amount to change happiness by, positive or negative"""
+        new_happiness = self.happiness + happiness_change
+        if new_happiness > 10:
+            new_happiness = 10
+        elif new_happiness < 0:
+            new_happiness = 0
+        self.happiness = new_happiness
 
     def sameLoc(self, other_character):
         return self.location == other_character.location
