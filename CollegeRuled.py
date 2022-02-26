a9985a43fef61e654adae26fd8818a6ae2d4946d
Nicholas Treynor <Ntreynor@gmail.com>
2022-02-25 23:59:52 -0800

    Should have committed multiple times. Code is now capable of chaining together events repeatedly. Adding function to events to grab only the reachable worldstate, without printing anything. TODOs added to mark areas of future work.

--- CollegeRuled.py
+++ CollegeRuled.py
@@ -114,12 +114,63 @@ if __name__ == "__main__":
 
 
     loveEvent = FallInLove()
-    preconditions_met, characters, environments = loveEvent.checkPreconditions(initialState)
-    if preconditions_met:
-        next_worldstate = loveEvent.doEvent(initialState, characters[0], environments)
+    #preconditions_met, characters, environments = loveEvent.checkPreconditions(initialState)
+    #if preconditions_met:
+    #    next_worldstate = loveEvent.doEvent(initialState, characters[0], environments)
 
-    #airlockEvent = VentThroughAirlock()
+    airlockEvent = VentThroughAirlock()
     #preconditions_met, characters = airlockEvent.checkPreconditions(initialState)
     #if preconditions_met:
     #    next_worldstate = airlockEvent.doEvent(initialState, characters[0], space)
+
+    possibleEvents = [loveEvent, airlockEvent]
+
+    run_story(initialState, possibleEvents, 5)
+
+def run_story(worldstate, possibleEvents, depthLimit):
+    if (depthLimit == 0):
+        return
+    runnableEvents = []
+    for event in possibleEvents: # Check to see if an instance of an event is runnable
+        preconditions_met, characters, environments = event.checkPreconditions(worldstate)
+        if preconditions_met: # If so, add all possible instances to the list of runnable events
+            print("length of characters: " + str(len(characters)))
+            for x in range(len(characters)):
+                print(x)
+                # Store the event, and it's parameters
+                runnableEvents.append([event, worldstate, characters[x], environments[x]])
+
+    # Now we would want to select an event to run.
+    desiredWorldState = worldstate # TODO: Replace this with an actual goal worldstate
+    indexOfEventToRun = selectEventIndex(runnableEvents, desiredWorldState)
+    print(indexOfEventToRun)
+    event = runnableEvents[indexOfEventToRun][0]
+    worldStateToRun = runnableEvents[indexOfEventToRun][1]
+    charsToUse = runnableEvents[indexOfEventToRun][2]
+    environmentsToUse = runnableEvents[indexOfEventToRun][3]
+    next_worldstate = event.doEvent(worldStateToRun, charsToUse, environmentsToUse)
+
+    run_story(next_worldstate, possibleEvents, depthLimit-1)
+    return
+
+def selectEventIndex(eventList, desiredWorldState):
+    print("attempting to select event index")
+    currMinDistanceEventIndex = -1
+    currEventMinDistance = 9999
+    print(len(eventList))
+    for x in range (len(eventList)):
+        print("x = " + str(x))
+        currEventValue = distanceBetweenWorldstates(desiredWorldState, eventList[x][0].getNewWorldState(eventList[x][1], eventList[x][2], eventList[x][3]))
+        if (currEventValue < currEventMinDistance):
+            currEventMinDistance = currEventValue
+            currMinDistanceEventIndex = x
+
+    return currMinDistanceEventIndex # Return the index of the event with the lowest distance to the desiredWorldState
+
+
+def distanceBetweenWorldstates(currWorldState, newWorldState):
+    #TODO: IMPLEMENT THIS PROPERLY
+    print("Distance between world states is 5")
+    return 5
+
 
--- events.py
+++ events.py
@@ -10,22 +10,27 @@ class PlotFragment:
         the characters involved, environments, and the updated drama score"""
         return
 
-    def doEvent(self, worldstate, characters):
+    def doEvent(self, worldstate, characters, environment):
+        return
+
+    def getNewWorldstate(self, worldstate, characters, environment):
         return
 
 class VentThroughAirlock(PlotFragment):
     def checkPreconditions(self, worldstate):
         valid_characters = []
+        environments = []
         for character in worldstate.characters:
             if character.location.has_airlock:
                 for character2 in character.relationships:
                     if (character.relationships[character2] < 0) & character.sameLoc(character2):
                         valid_characters.append([character, character2])
+                        environments.append([])
 
         if valid_characters:
-            return True, valid_characters, []
+            return True, valid_characters, environments
         else:
-            return False, None, []
+            return False, None, environments
 
     def doEvent(self, worldstate, characters, environment):
         reachable_worldstate = copy.deepcopy(worldstate)
@@ -34,18 +39,26 @@ class VentThroughAirlock(PlotFragment):
         reachable_worldstate.characters[worldstate.characters.index(characters[1])].location = environment #Change this in the future, environment is a copy (bc deepcopy)
         return reachable_worldstate
 
+    def getNewWorldState(self, worldstate, characters, environment):
+        reachable_worldstate = copy.deepcopy(worldstate)
+        reachable_worldstate.index += 1
+        reachable_worldstate.characters[worldstate.characters.index(characters[1])].location = environment #Change this in the future, environment is a copy (bc deepcopy)
+        return reachable_worldstate
+
 class FallInLove(PlotFragment):
     def checkPreconditions(self, worldstate):
         valid_characters = []
+        environments = []
         for character in worldstate.characters:
                 for character2 in character.relationships:
                     if (character.relationships[character2] > 0) & character.sameLoc(character2):
                         valid_characters.append([character, character2])
+                        environments.append([])
 
         if valid_characters:
-            return True, valid_characters, []
+            return True, valid_characters, environments
         else:
-            return False, None, []
+            return False, None, environments
 
     def doEvent(self, worldstate, characters, environment):
         reachable_worldstate = copy.deepcopy(worldstate)
@@ -58,6 +71,18 @@ class FallInLove(PlotFragment):
         print(char_one.relationships[char_two])
         char_one.relationships[char_two] += 15 #Change this in the future, environment is a copy (bc deepcopy)
         print(char_one.relationships[char_two])
+        return reachable_worldstate
+
+    def getNewWorldState(self, worldstate, characters, environment):
+        reachable_worldstate = copy.deepcopy(worldstate)
+        #print("{} looks at {} and realizes they have been slowly growing attached to them as they have spent time together. They feel a stronger desire to be around them".format(characters[0].name, characters[1].name))
+        char_one_index = worldstate.characters.index(characters[0])
+        char_two_index = worldstate.characters.index(characters[1])
+        char_one = reachable_worldstate.characters[char_one_index]
+        char_two = reachable_worldstate.characters[char_two_index]
+        #print(char_one.relationships[char_two])
+        char_one.relationships[char_two] += 15 #Change this in the future, environment is a copy (bc deepcopy)
+        #print(char_one.relationships[char_two])
         return reachable_worldstate
 
 
